c0d8578a4e6bfc03c652bd539d5688ee1a6dc75c
Jesse <1956125@cegep-heritage.qc.ca>
2022-03-24 09:49:47 -0400

    init

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace razor_ef_2022.Pages;
+
+public class PrivacyModel : PageModel
+{
+    public String Message {get; private set;} = "Message!";
+    private readonly ILogger<PrivacyModel> _logger;
+
+    public PrivacyModel(ILogger<PrivacyModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+        Message += $"Time is {DateTime.Now}";
+    }
+   
+}
+
